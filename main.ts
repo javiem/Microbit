0b662bac1faf6f6b177977e0809b6828ab22a9ae
Javier Esquiva Mira <javieresquivamira@gmail.com>
2021-10-26 12:40:31 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,66 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(20)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(20)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(20)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    basic.pause(20)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(20)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.pause(20)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(20)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(20)
+})
